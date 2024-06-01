19d08b97567623a14aa2399275cb77160217c958
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-06-02 01:20:43 +0300

    scatchy change(be careful here)

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,7 +1,11 @@
-// C:\Users\pavel.kuplensky\pokeprojectv2\src\app\[id]\page.tsx
-
 "use client";
 import React, { useState, useEffect } from "react";
+
+interface Ability {
+  ability: {
+    name: string;
+  };
+}
 
 interface PokemonDetail {
   name: string;
@@ -9,48 +13,47 @@ interface PokemonDetail {
   image: string;
 }
 
-const PokemonDetailPage: React.FC = ({ params }) => {
-  // const router = useRouter();
-  const pokeName = params.id;
-  const [pokemonDetail, setPokemonDetail] = useState<PokemonDetail | null>(
-    null
-  );
+interface Params {
+  id: string;
+}
+
+interface Props {
+  params: Params;
+}
+
+const PokemonDetailPage: React.FC<Props> = ({ params }) => {
+  const pokeName = params.id; 
+  const [pokemonDetail, setPokemonDetail] = useState<PokemonDetail | null>(null);
 
   useEffect(() => {
     async function getPokemonDetail() {
-      const response = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/${pokeName}`
-      );
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
       const data = await response.json();
       setPokemonDetail({
         name: data.name,
         abilities: data.abilities
-          .map((a: { ability: { name: string } }) => a.ability.name)
+          .map((a : Ability) => a.ability.name)
           .join(", "),
         image: data.sprites.front_default,
       });
     }
 
     getPokemonDetail();
-  }, [pokeName]);
+ }, [pokeName]);
 
-  return pokemonDetail ? (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-      }}
-    >
-      <img src={pokemonDetail.image} alt={pokemonDetail.name} />
-      <h1>{pokemonDetail.name}</h1>
-      <p>{pokemonDetail.abilities}</p>
-    </div>
-  ) : (
-    <div>Загрузка...</div>
-  );
+   return pokemonDetail ? (
+       <div style={{
+ display:"flex", 
+ flexDirection:"column", 
+ alignItems:"center", 
+ justifyContent:"center",
+ height:"100vh"
+}}>
+ <img src={pokemonDetail.image} alt={"Image of " + pokemonDetail.name} />
+<h1>{pokemonDetail.name}</h1>
+<p>{pokemonDetail.abilities}</p>
+
+ </div>) : (<div>Загрузка...</div>);
 };
 
 export default PokemonDetailPage;
