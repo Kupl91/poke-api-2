84f8936811b51553cd293633d56f2ec6ac54abe2
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-06-02 00:32:19 +0300

    create\route.ts fixed

--- src/app/api/pokemon/create/route.ts
+++ src/app/api/pokemon/create/route.ts
@@ -1,5 +1,4 @@
 // C:\Users\pavel.kuplensky\Documents\GitHub\poke-api-2\src\app\api\pokemon\create\route.ts
-
 import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
@@ -9,44 +8,47 @@ export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
     const { name, weight, height, species, experience, abilities = [] }: 
-          { name: string; weight: number; height: number; species: string; experience: number; abilities: string[] } = body;
+          { name: string; weight: number; height: number; species: string; experience: number; abilities: Array<{ abilityId: number }> } = body;
 
     if (!name || !species || isNaN(weight) || isNaN(height) || isNaN(experience)) {
-      return NextResponse.json({ message: "Неверные данные" }, { status: 400 });
-    }
+      return NextResponse.json({ message : "Неверные данные" },{status :400});
+}
+   
+        const existingPokemon = await prisma.pokemon.findUnique({
+        where:{name},
+       });
 
-    const existingPokemon = await prisma.pokemon.findUnique({
-      where: { name },
-    });
+if ( existingPokemon){
+return new Response(JSON.stringify({message :"Покемон с таким именем уже существует"}),{
+status :400});
+}
 
-    if (existingPokemon) {
-      return new Response(JSON.stringify({ message : "Покемон с таким именем уже существует" }),{status :400});
-       }
+           const newPokemon = await prisma.pokemon.create({
+            data:{
+              name,
+              weight:Number(weight),
+              height:Number(height),
+               species,
+               experience:Number(experience),
 
-   const newPokemon = await prisma.pokemon.create({
-       data:{ 
-        name,
-        weight:Number(weight),
-        height:Number(height),
-         species,
-         experience:Number(experience),
-               abilities:{
-           createMany:{
-              data :abilities.map((ability:string)=> ({ ability }))
-                }
-              }
-             }
-           });
+                    abilities:{
+                createMany:{
+                   data :abilities.map(({abilityId})=> ({ abilityId }))
+                      }
+                    }
+                   }
+                 });
 
-         return new Response(JSON.stringify(newPokemon),{
-           status :201
-
+return new Response(JSON.stringify(newPokemon),{
+         status :201
 });
 
 } catch (error ){
-console.error('Ошибка при создании нового покемона', error.message);
+const errorMessage= error instanceof Error ? error.message :'Неизвестная ошибка';
+console.error('Ошибка при создании нового покемона:',errorMessage);
 
-return new Response(JSON.stringify({message:error.message}),{
-status :500 });
-}
-}
+return new Response(JSON.stringify({message:errorMessage}),{
+status :500 
+      });
+     }
+   }
