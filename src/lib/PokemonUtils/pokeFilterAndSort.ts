ebdabb284d44084f67247bdcae29c0680a1c6ef7
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-06-03 01:05:34 +0300

    preprod fix

--- src/lib/PokemonUtils/pokeFilterAndSort.ts
+++ src/lib/PokemonUtils/pokeFilterAndSort.ts
@@ -1,25 +1,33 @@
 // C:\Users\Pavel\poke-api-2\src\lib\PokemonUtils\pokeFilterAndSort.ts
 import { useState } from 'react';
 
-export const usePokemonFilterAndSort = (pokemons) => {
-  const [sortType, setSortType] = useState('id');
-  const [filterType, setFilterType] = useState('name');
+interface Pokemon {
+  id: number;
+  name: string;
+  [key: string]: any; // Позволяет добавлять другие свойства
+}
+
+export const usePokemonFilterAndSort = (pokemons: Pokemon[]) => {
+  const [sortType, setSortType] = useState<keyof Pokemon>('id');
+  const [filterType, setFilterType] = useState<keyof Pokemon>('name');
   const [filterValue, setFilterValue] = useState('');
 
-  const handleSortChange = (value) => {
+  const handleSortChange = (value: keyof Pokemon) => {
     setSortType(value);
   };
 
-  const handleFilterTypeChange = (value) => {
+  const handleFilterTypeChange = (value: keyof Pokemon) => {
     setFilterType(value);
   };
 
-  const handleFilterValueChange = (event) => {
+  const handleFilterValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFilterValue(event.target.value.toLowerCase());
   };
 
   const sortedAndFilteredPokemons = pokemons
-    .filter((pokemon) => pokemon[filterType]?.toString().toLowerCase().includes(filterValue))
+    .filter((pokemon) =>
+      pokemon[filterType]?.toString().toLowerCase().includes(filterValue)
+    )
     .sort((a, b) => {
       if (sortType === 'name') {
         return a.name.localeCompare(b.name);
@@ -27,6 +35,7 @@ export const usePokemonFilterAndSort = (pokemons) => {
         return a[sortType] - b[sortType];
       }
     });
+
 
   return { 
     sortType, 
