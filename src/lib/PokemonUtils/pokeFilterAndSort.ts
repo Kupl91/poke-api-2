dbbaebff036ff50bbd7369bb4daea68116a00b4a
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-06-05 16:41:18 +0300

    asc desc sort

--- src/lib/PokemonUtils/pokeFilterAndSort.ts
+++ src/lib/PokemonUtils/pokeFilterAndSort.ts
@@ -1,4 +1,3 @@
-// C:\Users\Pavel\poke-api-2\src\lib\PokemonUtils\pokeFilterAndSort.ts
 import { useState } from 'react';
 import { Pokemon } from '@/lib/types';
 
@@ -6,9 +5,12 @@ export const usePokemonFilterAndSort = (pokemons: Pokemon[]) => {
   const [sortType, setSortType] = useState<keyof Pokemon>('id');
   const [filterType, setFilterType] = useState<keyof Pokemon>('name');
   const [filterValue, setFilterValue] = useState('');
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
 
   const handleSortChange = (value: keyof Pokemon) => {
     setSortType(value);
+    // При изменении типа сортировки меняем порядок на противоположный
+    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
   };
 
   const handleFilterTypeChange = (value: keyof Pokemon) => {
@@ -23,6 +25,7 @@ export const usePokemonFilterAndSort = (pokemons: Pokemon[]) => {
     sortType,
     filterType,
     filterValue,
+    sortOrder,
     handleSortChange,
     handleFilterTypeChange,
     handleFilterValueChange,
@@ -31,12 +34,14 @@ export const usePokemonFilterAndSort = (pokemons: Pokemon[]) => {
         (pokemon[filterType] as string | number)?.toString().toLowerCase().includes(filterValue)
       )
       .sort((a, b) => {
+        let comparison = 0;
         if (typeof a[sortType] === 'string' && typeof b[sortType] === 'string') {
-          return (a[sortType] as string).localeCompare(b[sortType] as string);
+          comparison = (a[sortType] as string).localeCompare(b[sortType] as string);
         } else if (typeof a[sortType] === 'number' && typeof b[sortType] === 'number') {
-          return (a[sortType] as number) - (b[sortType] as number);
+          comparison = (a[sortType] as number) - (b[sortType] as number);
         }
-        return 0;
+        // Если порядок сортировки 'desc', меняем знак сравнения
+        return sortOrder === 'desc' ? -comparison : comparison;
       })
   };
 };
