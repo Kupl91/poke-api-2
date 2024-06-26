320c247c4f0bd4928666049b9b914a7d10ace252
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-06-26 17:06:08 +0300

    -;

--- src/app/api/pokemon/delete/route.ts
+++ src/app/api/pokemon/delete/route.ts
@@ -1,16 +1,16 @@
 //C:\Users\pavel.kuplensky\Documents\GitHub\poke-api-2\src\app\api\pokemon\delete\route.ts
-import { NextRequest, NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
+import { NextRequest, NextResponse } from 'next/server'
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 export async function DELETE(req: NextRequest) {
-  const { searchParams } = new URL(req.url);
-  const id = searchParams.get('id');
+  const { searchParams } = new URL(req.url)
+  const id = searchParams.get('id')
 
   if (!id) {
     // Если ID не указан, возвращаем ошибку
-    return NextResponse.json({ error: 'ID не указан' }, { status: 400 });
+    return NextResponse.json({ error: 'ID не указан' }, { status: 400 })
   }
   try {
     await prisma.$transaction([
@@ -20,15 +20,15 @@ export async function DELETE(req: NextRequest) {
       prisma.pokemon.delete({
         where: { id: Number(id) },
       }),
-    ]);
+    ])
 
-    return NextResponse.json({ message: 'Покемон успешно удален' }, { status: 200 });
+    return NextResponse.json({ message: 'Покемон успешно удален' }, { status: 200 })
   } catch (error) {
-    const errorMessage = error instanceof Error ? error.message : 'Неизвестная ошибка';
-    console.error('Ошибка при удалении покемона:', errorMessage);
+    const errorMessage = error instanceof Error ? error.message : 'Неизвестная ошибка'
+    console.error('Ошибка при удалении покемона:', errorMessage)
 
     return new Response(JSON.stringify({ error: 'Не удалось удалить покемона', details: errorMessage }), {
       status: 500,
-    });
+    })
   }
 }
